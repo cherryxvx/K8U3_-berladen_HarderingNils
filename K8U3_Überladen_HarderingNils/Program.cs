e67a5db68a28fa39fc5dab56f15821d8156d3da1
cherry <hardering64@hotmail.com>
2020-09-17 19:54:39 +0200

    Add project files.

---
+++ K8U3_Überladen_HarderingNils/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace K8U3_Überladen_HarderingNils
+{
+    class Wert
+    {
+        public void Ausgabe(int number)
+        {
+            Console.WriteLine("Die Ganzzahl lautet: " + number);
+        }
+
+        public void Ausgabe(double value)
+        {
+            Console.WriteLine("Die Dezimalzahl lautet: " + value);
+        }
+
+        public void Ausgabe(string txt)
+        {
+            Console.WriteLine("Der Text lautet: " + txt);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wert wertObjekt = new Wert();
+
+            wertObjekt.Ausgabe(12345);
+            wertObjekt.Ausgabe(12345.12345);
+            wertObjekt.Ausgabe("Test");
+            Console.ReadLine();
+        }
+    }
+}
